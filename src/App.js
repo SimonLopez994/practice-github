5b52c13e5b8995c83ce1fccfb0a749632e24ea4f
Simon <chimon994@gmail.com>
2024-02-07 16:14:51 -0300

    cards and card-item components after styles

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ const App = () => {
     {
       id: 1,
       name: 'Juan',
-      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s',
+      imageUrl:'https://previews.123rf.com/images/phaianalog/phaianalog1711/phaianalog171100102/89870777-vector-de-fondo-formas-geom%C3%A9tricas-dise%C3%B1o-moderno-en-el-concepto-de-arte.jpg'
     },
     {
       id: 2,
       name: 'Simon',
-      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s',
+      imageUrl:'https://previews.123rf.com/images/phaianalog/phaianalog1711/phaianalog171100102/89870777-vector-de-fondo-formas-geom%C3%A9tricas-dise%C3%B1o-moderno-en-el-concepto-de-arte.jpg'
     },
     {
       id: 3,
       name: 'Lolo',
-      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged'
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s',
+      imageUrl:'https://previews.123rf.com/images/phaianalog/phaianalog1711/phaianalog171100102/89870777-vector-de-fondo-formas-geom%C3%A9tricas-dise%C3%B1o-moderno-en-el-concepto-de-arte.jpg'
     },
   ]
   return (
--- src/components/card-item/card-item.component.jsx
+++ src/components/card-item/card-item.component.jsx
@@ -1,10 +1,15 @@
 import './card-item.styles.scss';
 const CardItem = ({ user }) => {
-    const { id, name, paragraph } = user; 
+    const { id, name, paragraph, imageUrl } = user;
     return (
         <div key={id} className="card">
-            <h2>{name}</h2>
-            <p>{paragraph}</p>
+            <div className='img' style={{ backgroundImage: `url(${imageUrl})` }}>
+                <div className='card-content'>
+                    <h2>{name}</h2>
+                    <p>{paragraph}</p>
+                </div>
+            </div>
+
         </div>
     )
 }
